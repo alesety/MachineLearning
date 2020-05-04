d2f2f4e38e745498ca23f4345483227caaf0fe6a
alesety <alesety535@gmail.com>
2020-05-04 14:30:40 +0900

    fix mini batch

--- deep-learning-from-scratch/ch04/cross_entropy_error.py
+++ deep-learning-from-scratch/ch04/cross_entropy_error.py
@@ -1,8 +1,13 @@
 import numpy as np
 
 def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+    
+    batch_size = y.shape[0]
     delta = 1e-7 # 対数の真数が0のとき-infになるので微小な値を足す
-    return -np.sum(t * np.log(y + delta))
+    return -np.sum(t * np.log(y + delta)) / batch_size
 
 # 2が正解
 t = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
@@ -13,4 +18,14 @@ print(cross_entropy_error(np.array(y), np.array(t)))
 
 # 7の確率が最も高い
 y = [0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0]
+print(cross_entropy_error(np.array(y), np.array(t))) 
+
+# ミニバッチ対応
+t = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+t = [t, t]
+
+y = [
+    [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0], # 2の確率が最も高い
+    [0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0]  # 7の確率が最も高い
+]
 print(cross_entropy_error(np.array(y), np.array(t))) 
--- deep-learning-from-scratch/ch04/mean_squared_error.py
+++ deep-learning-from-scratch/ch04/mean_squared_error.py
@@ -2,7 +2,12 @@ import numpy as np
 
 # 二乗和誤差
 def mean_squared_error(y, t):
-    return 0.5 * np.sum((y - t) ** 2)
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+    
+    batch_size = y.shape[0]
+    return 0.5 * np.sum((y - t) ** 2) / batch_size
 
 # 2が正解
 t = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
@@ -13,4 +18,14 @@ print(mean_squared_error(np.array(y), np.array(t)))
 
 # 7の確率が最も高い
 y = [0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0]
+print(mean_squared_error(np.array(y), np.array(t))) 
+
+# ミニバッチ対応
+t = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+t = [t, t]
+
+y = [
+    [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0], # 2の確率が最も高い
+    [0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0]  # 7の確率が最も高い
+]
 print(mean_squared_error(np.array(y), np.array(t))) 
